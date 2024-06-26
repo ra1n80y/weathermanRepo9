094000ff02367ffad871018fd3c57fdda76c8a90
ra1n80y <godfreyachu1@gmail.com>
2024-06-26 16:54:59 +0200

    Removed unused import stmt

--- src/main/java/com/WEATHER/ApacheKafkaSpringBootApplication.java
+++ src/main/java/com/WEATHER/ApacheKafkaSpringBootApplication.java
@@ -1,6 +1,5 @@
 package com.WEATHER;
 
-import com.WEATHER.model.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
